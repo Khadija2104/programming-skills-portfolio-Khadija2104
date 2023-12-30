908cc13925fdbbe16d7180ed172c85c9b9ac52ef
Khadija2104 <144886400+Khadija2104@users.noreply.github.com>
2023-12-30 22:59:05 +0400

    Create Exercise4.py
    exercise 4

---
+++ Chapter 1- Getting Started with Python/Exercise4.py
@@ -0,0 +1,6 @@
+#write three strings in different variables and print the output as one string.
+string1=" Asalamulaikum"
+string2=" HI "
+string3=" How are you?"
+output_string= string1 + string2 + string3
+print(output_string)
